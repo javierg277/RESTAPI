1a1edeee28004022881390f40a58e1e9c7cf32d3
javierg277 <119664848+javierg277@users.noreply.github.com>
2024-02-14 11:25:22 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,19 +1,14 @@
-<<<<<<< HEAD
-using CajeroAPI.Models;
-using Elfie.Serialization;
-using Microsoft.Data.SqlClient;
-using Oracle.ManagedDataAccess.Client;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-
-=======
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
->>>>>>> dfbb659c1e1f3e366d5606281d5b6cb6d0e4b404
+using Sepalo.WebApi.Admin.Repository;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<BaseRepository>();
+
 // Configure JWT authentication
 var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 var key = Encoding.ASCII.GetBytes(tokenOptions.SecretKey);
@@ -34,38 +29,23 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-var app = builder.Build();
 
-OracleConnection _dbConnection = new OracleConnection("Data Source = api;User Id = system; Password = denigrancia;");
-try
-{
-    _dbConnection.Open();
-    Console.WriteLine("Connection to database established successfully.");
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Error connecting to database: {ex.Message}");
-}
-finally
-{
-    _dbConnection.Close();
-}
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthentication(); // Add this line
+app.UseAuthentication();
 app.UseAuthorization();
+app.MapControllers();
+app.Run();
 
 app.MapControllers();
 
